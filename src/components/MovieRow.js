47705a0d7107487e1b95bf450d1900b7b66d65e7
Marcos <marcosfirmino.009@gmail.com>
2021-12-18 16:41:48 -0300

    Ajustes no botão da listas de filmes/séries

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -2,13 +2,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from 'react'
 import './MovieRow.css'
+import ArrowLeft from '../assets/arrow-left.svg'
+import ArrowRight from '../assets/arrow-right.svg'
 
 export default ({ title, items }) => {
-
   const [scrollX, setScrollX] = useState(30)
   const handleLeftArrow = () => {
     let x = scrollX + Math.round(window.innerWidth / 2)
-    if(x > 0) {
+    if (x > 0) {
       x = 0
     }
     setScrollX(x)
@@ -16,8 +17,8 @@ export default ({ title, items }) => {
   const handleRightArrow = () => {
     let x = scrollX - Math.round(window.innerWidth / 2)
     let listW = items.results.length * 150
-    if((window.innerWidth - listW) > x) {
-      x = (window.innerWidth - listW) - 60
+    if (window.innerWidth - listW > x) {
+      x = window.innerWidth - listW - 60
     }
     setScrollX(x)
   }
@@ -25,18 +26,25 @@ export default ({ title, items }) => {
     <div className="movieRow">
       <h2>{title}</h2>
       <div className="movieRow--left" onClick={handleLeftArrow}>
-        <img src="https://cdn4.iconfinder.com/data/icons/multimedia-75/512/multimedia-04-512.png" style={{width:50}}/>
+        <img src={ArrowLeft}/> 
       </div>
       <div className="movieRow--right" onClick={handleRightArrow}>
-        <img src="https://cdn4.iconfinder.com/data/icons/multimedia-75/512/multimedia-03-512.png" style={{width:50}}/>
+        <img src= {ArrowRight}/>
       </div>
       <div className="movieRow--listarea">
-        <div className="movieRow--list" style={{marginLeft: scrollX, width: items.results.length * 150}}>
-        {items.results.length > 0 && items.results.map((item, key) => (
-          <div className="movieRow--item">
-            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
-          </div>
-          ))}
+        <div
+          className="movieRow--list"
+          style={{ marginLeft: scrollX, width: items.results.length * 150 }}
+        >
+          {items.results.length > 0 &&
+            items.results.map((item, key) => (
+              <div className="movieRow--item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_title}
+                />
+              </div>
+            ))}
         </div>
       </div>
     </div>
